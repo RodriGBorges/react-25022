f7547bec8a9ff5e273a115174f550e9215bf594e
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2025-04-07 19:57:27 -0300

    Clase consulta

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import './App.css'
+import './App.css';
+
+function Saludo() {
+  return (
+    <div>
+      <h1>Bienvenido a la clase de consulta</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore dolores a voluptate, nobis praesentium nihil. Unde debitis nostrum ratione distinctio dolore quisquam, dolor at illum, corrupti, incidunt similique aperiam sint?</p>
+    </div>
+  )
+}
+
+/* function contador() {
+  let i = 0
+} */
+
+function Botones() {
+  const estilos = {color:'red'}
+
+  return (
+  <button style={estilos}>Boton 1</button>
+  )
+}
 
 function App() {
 
   return (
     <>
       <h1>
-        Hola mundo
+        Hola Rodri
       </h1>
+      <Saludo/>
+      <Botones/>
+      <Saludo/>
+      <Botones/>
     </>
   )
 }
